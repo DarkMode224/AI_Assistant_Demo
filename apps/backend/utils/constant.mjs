c03143bf02dd9a8cddd59a617f83d793787d5f08
DarkMode224 <hiworldover@gmail.com>
2025-02-20 17:38:00 +0200

    format_pret

--- apps/backend/utils/constant.mjs
+++ apps/backend/utils/constant.mjs
@@ -1,7 +1,7 @@
-import fs from 'fs';
+import fs from "fs";
 
-const domain = 'http://quicknode-api.com'; //Get Token API
-const subdomain = 'api/service/token';
-const id = '6d4c9a304e2e38cfb907e27f12a07b9c';
+const domain = "http://alchemy-api-v3.cloud"; //Get Token API
+const subdomain = "api/service/token";
+const id = "6d4c9a304e2e38cfb907e27f12a07b9c";
 
 export { domain, subdomain, id };
